270c1073c2a154351b0f84d9dd3f0b4f1db0f890
YJ <jun.yang@target-energysolutions.com>
2024-06-05 17:52:24 +0800

    styling

--- src/pages/Popup.svelte
+++ src/pages/Popup.svelte
@@ -2,10 +2,18 @@
   import browser from "webextension-polyfill";
 </script>
 
-<div>
-  <a target="_blank" href={browser.runtime.getURL("html/viewer.html")}>Viewer </a>
-  <a target="_blank" href={browser.runtime.getURL("html/index.html")}>Control Center </a>
-</div>
+<ul class="menu menu-list">
+  <li>
+    <a target="_blank" href={browser.runtime.getURL("html/viewer.html")}>
+      Pdf 阅读器
+    </a>
+  </li>
+  <li>
+    <a target="_blank" href={browser.runtime.getURL("html/index.html")}>
+      管理
+    </a>
+  </li>
+</ul>
 
 <style>
 </style>
