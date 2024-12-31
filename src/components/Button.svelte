cb04330fd8f89dd7b77d9e2c53783f780ed61a29
YJ <jun.yang@target-energysolutions.com>
2024-12-31 11:07:11 +0800

    show loading

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,16 +3,36 @@
 
   let {
     variant = "primary",
+    onclick: onclick,
     ...rest
   }: HTMLButtonAttributes & {
     variant?: "primary" | "primary-outline";
+    onclick?: (e: MouseEvent) => void | Promise<void>;
   } = $props();
+
+  let isLoading = $state(false);
+
   let cls = $derived(
-    `button ${variant === "primary" ? "is-primary" : "primary-outline"}`,
+    `button ${variant === "primary" ? "is-primary" : "primary-outline"} ${isLoading ? "is-loading" : ""}`,
   );
+
+  async function handleClick(e: MouseEvent) {
+    if (onclick) {
+      try {
+        isLoading = true;
+        await onclick(e);
+      } finally {
+        isLoading = false;
+      }
+    }
+  }
 </script>
 
-<button class={cls} {...rest}>
+<button class={cls} 
+  {...rest}
+  onclick={handleClick}
+  disabled={isLoading || rest.disabled}
+>
   <slot />
 </button>
 
